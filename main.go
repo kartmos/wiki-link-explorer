ffd1649cf6f8ea077296062c58c36c5338512631
kartmos <kartemgithub@gmail.com>
2025-02-12 15:40:16 +0300

    freezing progarm on 2 parsing

--- main.go
+++ main.go
@@ -2,22 +2,24 @@ package main
 
 import (
 	"bufio"
+	"context"
 	"fmt"
 	"io"
 	"net/http"
 	"regexp"
 	"strings"
+	"sync"
+	"time"
 )
 
 type Param struct {
-	NumberMap   int
 	InputURL    string
+	NumberMap   int
 	MatchURL    string
 	MatchWord   string
-	DoMatch     bool
+	BoolMatch   bool
 	CountTreads int
 	Storage     chan interface{}
-	input       chan string
 	output      chan string
 }
 
@@ -31,41 +33,93 @@ func NewParser(param Param) *Parser {
 	}
 }
 
-func (v *Parser) pusher(input chan interface{}) {
-	fmt.Println("Start pusher")
-	for msg := range input {
-		v.accretionMap(msg)
-	}
+func (v *Parser) work(data map[int]string) {
 
+	fmt.Println("New Work")
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+	buffer := make(map[int]string)
+	done := make(chan struct{})
+	bridge := make(chan string)
+	stringParseChan := make(chan string)
+	wg := &sync.WaitGroup{}
+
+	time.Sleep(2 * time.Second)
+	for i := 0; i < v.param.CountTreads; i++ {
+		fmt.Printf("Start worker ------------>  %d\n\n", i)
+		wg.Add(1)
+		go v.run(ctx, wg, stringParseChan, bridge)
+	}
+	go v.accumulator(buffer, wg, done, bridge)
+	go func() {
+		fmt.Println("Sending string in workerpool...")
+		defer fmt.Println("Sended all string in workerpool ---> stop pusher")
+		for _, val := range data {
+			stringParseChan <- val
+		}
+		var stop struct{}
+		done <- stop
+		close(stringParseChan)
+	}()
+
+	wg.Wait()
+	time.Sleep(1 * time.Second)
+	fmt.Println("Accumulator stop work")
+	close(done)
+	close(bridge)
+
+	select {
+	case result := <-v.param.output:
+		fmt.Printf("\n\n\nMatched ->>\n\n%s\n", result)
+		return
+	default:
+	}
+	newData := v.accretion(v.param.Storage)
+	time.Sleep(5 * time.Second)
+	fmt.Println("Finish work\n\n\n\n")
+
+	v.work(newData)
 }
 
-func (v *Parser) accretionMap(data interface{}) {
-	if m, ok := data.(map[int]string); ok {
-		v.mapRunner(m)
+func (v *Parser) accretion(input chan interface{}) map[int]string {
+	fmt.Println("Start accretion")
+	data := <-input
+	val := data.(map[int]string)
+	return val
+}
+
+func (v *Parser) accumulator(m map[int]string, wg *sync.WaitGroup, done chan struct{}, input chan string) {
+	fmt.Println("Accumulator start work")
+	idx := 0
+	for {
+		select {
+		case <-done:
+			v.param.Storage <- m
+			fmt.Println("SENDED map in Storage")
+			break
+		case str := <-input:
+			m[idx] = str
+			idx++
+		}
 	}
 }
 
-func (v *Parser) mapRunner(data map[int]string) {
-	PusherIdx := 0
-	if x, found := data[PusherIdx]; found {
-		fmt.Printf("URL in pusher:\n%s\n\n", x)
-		v.param.input <- x
-		PusherIdx++
-	}
-
-}
-func (v *Parser) run(input chan string) {
-	fmt.Println("Start worker")
-	for msg := range input {
-		v.parserUrl(msg)
+func (v *Parser) run(ctx context.Context, wg *sync.WaitGroup, input chan string, bridge chan string) {
+	defer fmt.Println("Stop worker")
+	defer wg.Done()
+	for str := range input {
+		select {
+		case <-ctx.Done():
+			return
+		default:
+			v.parserUrl(ctx, str, bridge)
+		}
 	}
 }
 
-func (v *Parser) parserUrl(s string) {
-	fmt.Println("Create Goroutine")
+func (v *Parser) parserUrl(ctx context.Context, s string, bridge chan string) {
+	// fmt.Println(s)
 	v.param.NumberMap++
-	linksBuffer := make(map[int]string)
-	mapIndex := 0
 
 	var client http.Client
 
@@ -73,6 +127,7 @@ func (v *Parser) parserUrl(s string) {
 	if err != nil {
 		fmt.Printf("Request error -> %s\n", err)
 	}
+
 	defer response.Body.Close()
 
 	body, err := io.ReadAll(response.Body)
@@ -85,46 +140,36 @@ func (v *Parser) parserUrl(s string) {
 	scanner := bufio.NewScanner(strings.NewReader(res))
 	for scanner.Scan() {
 		line := scanner.Text()
-		v.finder(line, linksBuffer, &mapIndex)
+		v.finder(ctx, line, bridge)
 	}
-	v.param.Storage <- linksBuffer
-	fmt.Printf("\n\n-> %d Map in Chan\n\n", v.param.NumberMap)
+	// fmt.Println("End parse inputURL")
 }
 
-func (v *Parser) finder(s string, buffer map[int]string, idx *int) error {
-	if !v.param.DoMatch {
-		return nil
-	}
+func (v *Parser) finder(ctx context.Context, s string, buffer chan string) {
 	if strings.Index(s, "/wiki/") >= 6 {
 		re := regexp.MustCompile(`href="/wiki/([^"]+)"`)
 		match := re.FindAllStringSubmatch(s, -1)
 		for _, element := range match {
-			if !v.match(element[1]) {
-				buffer[*idx] = "https://en.wikipedia.org/wiki/" + string(element[1])
-				*idx++
+			if element[1] == v.param.MatchWord {
+				v.param.BoolMatch = true
+				v.param.output <- "https://en.wikipedia.org/wiki/" + element[1]
+				close(v.param.Storage)
+				close(v.param.output)
 			} else {
-				close(v.param.Storage) // тут надо все остановить
+				// fmt.Println("https://en.wikipedia.org/wiki/" + element[1])
+				buffer <- "https://en.wikipedia.org/wiki/" + element[1]
 			}
 		}
 	}
-	return nil
 }
 
-func (v *Parser) match(s string) bool {
-	if s == v.param.MatchWord {
-		v.param.DoMatch = true
-		fmt.Printf("\n\n\n\n FOUND --------> %t\n\n\n\n", v.param.DoMatch)
-	}
-	return v.param.DoMatch
-}
-
-func (v *Parser) setupInitialData() {
+func (v *Parser) setupInitialData() map[int]string {
 	field := strings.Split(v.param.MatchURL, "/")
 	v.param.MatchWord = field[len(field)-1]
 	InitMap := make(map[int]string)
 	InitMap[0] = v.param.InputURL
-	v.param.Storage <- InitMap
 	fmt.Printf("Init URL ---> %s\n\n", v.param.InputURL)
+	return InitMap
 }
 
 func main() {
@@ -132,26 +177,20 @@ func main() {
 	p := NewParser(Param{
 		NumberMap:   0,
 		InputURL:    "https://en.wikipedia.org/wiki/World",
-		MatchURL:    "https://en.wikipedia.org/wiki/Académie_Française",
+		MatchURL:    "https://en.wikipedia.org/wiki/War",
 		CountTreads: 4,
-		Storage:     make(chan interface{}),
-		input:       make(chan string),
+		Storage:     make(chan interface{}, 1),
 		output:      make(chan string),
-		DoMatch:     false,
+		BoolMatch:   false,
 	})
+	start := p.setupInitialData()
+	p.work(start)
+	p.param.Storage <- start
 
-	p.setupInitialData()
-
-	for i := 0; i < p.param.CountTreads; i++ {
-		go p.run(p.param.input)
+	select {
+	case result := <-p.param.output:
+		fmt.Printf("\n\n\nMatched ->>\n\n%s\n", result)
+	case <-time.After(10 * time.Minute):
+		fmt.Println("Timeout reached, stopping...")
 	}
-
-	go p.pusher(p.param.Storage)
-
-	close(p.param.output)
-
-	for result := range p.param.output {
-		fmt.Printf("\n\n\nMatched ->>\n\n%s", result)
-	}
-	fmt.Scan()
 }
